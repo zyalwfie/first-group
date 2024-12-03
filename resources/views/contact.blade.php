bacfa25d5f4b6565995f5645f4ea6d799218eba5
zyalwfie <zyalwfie@gmail>
2024-12-03 22:07:43 +0800

    feat(ui): create scratch for contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,8 +5,31 @@
 
     <x-header></x-header>
 
-    <main class="my-10 px-4 lg:px-40 xl:px-72">
-        <h1>Contact Page</h1>
+    <main class="mt-24 my-10 px-4 lg:px-40 xl:px-72">
+        <section class="flex flex-col gap-2">
+            <div class="flex">
+                <h1>Contact Us</h1>
+                <p>Email, call, or complete the form to learn how firstGroup can solve your messaging problem</p>
+                <address>
+                    <span>fristgroup@gmail.com</span>
+                    <span>+62 82233344</span>
+                </address>
+            </div>
+            <div class="flex">
+                <div class="flex flex-col gap-2">
+                    <h3>Customer Support</h3>
+                    <p>Our support team is available second the clock to address any concerns or queries you may have.</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <h3>Feedback and Suggestions</h3>
+                    <p>We value your feedback and are continuously working to improve firstGroup. Your input is crucial in shaping the future of firstGroup.</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <h3>Media Queries</h3>
+                    <p>We value your feedback and are continuously working to improve firstGroup. Your input is crucial in shaping the future of firstGroup.</p>
+                </div>
+            </div>
+        </section>
     </main>
     
     <x-footer></x-footer>
